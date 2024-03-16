f803c646bd829e4fa7246baf5986b61b1d9b832e
Alfoldi Tibor <tibi@smartlabs.be>
2024-03-16 19:33:33 +0200

    cel mai apropiat flag

---
+++ .angular/cache/17.1.1/vite/deps/@angular_material_select.js
@@ -0,0 +1,55 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-LLT2DCKG.js";
+import "./chunk-VVBOFB34.js";
+import "./chunk-4NGGGARH.js";
+import "./chunk-QDNIZKEN.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-WI6OWWMH.js";
+import "./chunk-K6ZOHHLC.js";
+import "./chunk-W2PW3FTC.js";
+import "./chunk-Q26MDAIO.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-Q5IRRH7H.js";
+import "./chunk-YX3563TO.js";
+import "./chunk-2V5SS7FU.js";
+import "./chunk-WSA2QMXP.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
