74c64f8268eb6e8697b33394cc8a9e763b8faf3a
Alfoldi Tibor <tibi@smartlabs.be>
2024-03-16 20:27:44 +0200

    ertem.igen.

--- server.ts
+++ server.ts
@@ -1,55 +1,46 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
-import AppServerModule from './src/main.server';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { AppServerModule } from './src/main.server';
 
-// The Express app is exported so that it can be used by serverless Functions.
+// A szerveralkalmazás exportálása, hogy használható legyen szerver nélküli funkciókban.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+  const indexHtml = join(serverDistFolder, 'index.html'); // Győződj meg róla, hogy ez az útvonal megfelelő
 
-  const commonEngine = new CommonEngine();
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  // Example Express Rest API endpoints
+  // Példa Express Rest API végpontokra
   // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
+  // Statikus fájlok kiszolgálása a /browser mappából
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
 
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap: AppServerModule,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+  // Minden szabályos útvonal az Angular motor használatával
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
 
   return server;
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env.PORT || 4000;
 
-  // Start up the Node server
+  // A Node szerver elindítása
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Express szerver hallgatózik a következőn: http://localhost:${port}`);
   });
 }
 
